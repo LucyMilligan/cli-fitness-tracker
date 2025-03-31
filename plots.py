735a62ec611d5da3e7a5132c94f7937c1dd8f659
Lucy Milligan <lucyjmilligan@yahoo.co.uk>
2025-03-31 12:33:31 +0100

    add additional functions and notes to plot.py - using pandas and matplotlib

--- plots.py
+++ plots.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 from sqlmodel import Session
 from main import engine, SessionDep
 from models import Activity
+from datetime import datetime
+import pandas as pd
 
 load_dotenv()
 
@@ -14,18 +16,12 @@ postgres_url = os.getenv("DB_URL")
 engine = create_engine(postgres_url)
 
 
-#simple plot with no meaning
-plt.plot([1, 2, 3, 4, 5], [1, 4, 9, 16, 25], "ro")
-plt.axis((0, 6, 0, 30)) #xmin, xmax, ymin, ymax
-plt.ylabel("some numbers")
-plt.xlabel("some more numbers")
-# plt.show()
-
 def calculate_time_secs(moving_time: str):
     """calculates the moving time in seconds, given an input moving_time in the format
     "HH:MM:SS. Returns the time in secs as an integer."
     """
-    time_secs = int(moving_time[6:]) + (int(moving_time[3:5]) * 60) + (int(moving_time[0:2]) * 60 * 60)
+    hours, minutes, seconds = map(int, moving_time.split(":"))
+    time_secs = seconds + (minutes * 60) + (hours * 60 * 60)
     return time_secs
 
 
@@ -40,16 +36,114 @@ def calculate_speed_mins_per_km(distance: float, moving_time: str):
     return f"{speed_mins}:{str(speed_secs).rjust(2, "0")}"
 
 
+def convert_speed_to_float(speed_string: str):
+    """takes a speed string in the format "MM:SS" and converts it into a float.
+    e.g. "6:53" would return 6.88"""
+    minutes, seconds = map(int, speed_string.split(":"))
+    return round((minutes + (seconds / 60)), 2)
+
+
+def format_query_output(data, col_names):
+    """formats query output results into a dictionary.
+    
+    :param data: list of tuples containing the data, with each tuple representing a row
+    :param col_names: list of columns names relating to the data
+    :returns: a list of dictionaries containing the data in the format
+    [{col_1: data_1, col_2: data_2, col_3: data_3}]
+    """
+    formatted_data = []
+    for row in data:
+        formatted_data.append(dict(zip(col_names, row)))
+    return formatted_data
+
+
+#TODO: change so that all data is selected - can be cut down in the dataframes
 def select_activity_data(user_id: int, start_date: str = "1981/01/01", end_date: str = "2081/01/01"):
+    """queries the database and returns a list of dictionaries containing the query results.
+    
+    :param user_id: user id integer
+    :param start_date: earliest data for which activity data should be obtained.
+    This should be in a string of format "YYYY/MM/DD".
+    :param end_date: latest data for which activity data should be obtained.
+    This should be in a string of format "YYYY/MM/DD".
+    :returns: a list of dictionaries containing the activity data, with each dictionary representing 
+    a row of data, keys representing the column name and values representing the data.
+    """
     with Session(engine) as session:
         stmt = select(
-            Activity.id, Activity.distance_km, Activity.moving_time, Activity.date).where(
+            Activity.user_id, Activity.id, Activity.distance_km, Activity.moving_time, Activity.date).where(
                 Activity.user_id == user_id).where(
                     Activity.date > start_date, Activity.date < end_date)
-        activities = session.exec(stmt).all()
-    
-    print(activities)
-    print(stmt)
-    
+        activities = session.exec(stmt)
+        activities_data = session.exec(stmt).all()
+        activities_col_names = activities.keys()
 
-select_activity_data(1, "2025/03/01", "2025/03/20")
+    formatted_activities = format_query_output(activities_data, activities_col_names)
+    
+    return formatted_activities
+
+#TODO: test this
+def plot_pace_vs_date(user_id: int, start_date: str = "1981/01/01", end_date: str = "2081/01/01"):
+    activities = select_activity_data(user_id, start_date, end_date)
+    df = pd.DataFrame(activities)
+    df["date"] = pd.to_datetime(df["date"], format="%Y/%m/%d", errors="coerce")
+    df["pace"] = df.apply(lambda x: calculate_speed_mins_per_km(x["distance_km"], x["moving_time"]), axis=1)
+    df["speed_numeric"] = df["speed"].apply(convert_speed_to_float)
+
+    plt.figure(figsize=(12,6))
+    plt.plot(df["date"], df["speed_numeric"], marker="o", color="purple")
+    plt.xlabel("Date")
+    plt.ylabel("Pace (min/km)")
+    plt.gca().invert_yaxis() #slower speeds at the bottom
+    plt.grid(True)
+    plt.xticks(rotation=90)
+    plt.show()
+
+
+#plots:
+    #pace vs date
+    #pace vs distance
+    #pace vs elevation
+
+
+##################### NOTES / TRYING THINGS OUT - TO REMOVE ########################
+# ##### creating a dataframe from a dictionary
+# activities = select_activity_data(1)
+# df = pd.DataFrame(activities)
+
+# ##### filtering a dataframe
+# new_df = df.filter(["distance_km", "moving_time", "date"])
+
+# ##### sorting a dataframe
+# sorted_new_df = new_df.sort_values(by=["date"], ascending=True)
+
+# ##### creating a speed column to an existing df
+# df["speed"] = df.apply(lambda x: calculate_speed_mins_per_km(x["distance_km"], x["moving_time"]), axis=1)
+
+# ##### converting date string into dateformat (YYYY-MM-DD) on existing df
+# df["date"] = pd.to_datetime(df["date"], format="%Y/%m/%d", errors="coerce")
+
+# ##### plot date vs speed
+# date_speed_df = df.filter(["date", "speed"])
+# date_speed_df["speed_numeric"] = date_speed_df["speed"].apply(convert_speed_to_float)
+# plt.figure(figsize=(12,6))
+# plt.plot(date_speed_df["date"], date_speed_df["speed_numeric"], marker="o", color="purple")
+# plt.xlabel("Date")
+# plt.ylabel("Pace (min/km)")
+# plt.gca().invert_yaxis() #slower speeds at the bottom
+# plt.grid(True)
+# plt.xticks(rotation=90)
+# plt.show()
+
+# ##### to get a date from a string:
+# my_date = "2025/03/31"
+# my_date_object = datetime.strptime(my_date, "%Y/%m/%d").date()
+# print(my_date_object)
+# print(type(my_date_object))
+
+# ##### simple plot with no meaning
+# plt.plot([1, 2, 3, 4, 5], [1, 4, 9, 16, 25], "ro")
+# plt.axis((0, 6, 0, 30)) #xmin, xmax, ymin, ymax
+# plt.ylabel("some numbers")
+# plt.xlabel("some more numbers")
+# plt.show()
--- test_plots.py
+++ test_plots.py
@@ -1,4 +1,4 @@
-from plots import calculate_speed_mins_per_km, calculate_time_secs
+from plots import calculate_speed_mins_per_km, calculate_time_secs, convert_speed_to_float, format_query_output, select_activity_data
 
 class TestCalculateTimeSecs:
     def test_calculate_time_secs_just_secs(self):
@@ -34,7 +34,6 @@ class TestCalculateSpeed:
         expected = "6:36"
         result = calculate_speed_mins_per_km(distance, moving_time)
         assert result == expected
-
     
     def test_calculate_speed_min_per_km_greater_than_10_mins_per_km(self):
         moving_time = "02:15:30"
@@ -42,3 +41,49 @@ class TestCalculateSpeed:
         expected = "27:06"
         result = calculate_speed_mins_per_km(distance, moving_time)
         assert result == expected
+
+
+class TestConvertSpeedToFloat:
+    def test_convert_speed_to_float(self):
+        speed_string = "6:53"
+        result = convert_speed_to_float(speed_string)
+        assert result == 6.88
+
+
+class TestFormatQueryOutput:
+    def test_format_query_output_single_row(self):
+        data = [("test1", "test2", "test3")]
+        col_names = ["col1", "col2", "col3"]
+        result = format_query_output(data, col_names)
+        assert result == [{"col1": "test1", "col2": "test2", "col3": "test3"}]
+
+    def test_format_query_output_multiple_rows(self):
+        data = [("test1", "test2"), ("test3", "test4")]
+        col_names = ["col1", "col2"]
+        result = format_query_output(data, col_names)
+        assert result == [{"col1": "test1", "col2": "test2"}, {"col1": "test3", "col2": "test4"}]
+
+
+class TestSelectActivityData:
+    def test_select_activity_data_default_value_arguments(self):
+        user_id = 1
+        result = select_activity_data(user_id)
+        assert len(result) > 1
+        assert isinstance(result, list)
+        for item in result:
+            assert isinstance(item, dict)
+            assert isinstance(item["id"], int)
+            assert isinstance(item["distance_km"], float)
+            assert isinstance(item["moving_time"], str)
+
+    def test_select_activity_data_date_range(self):
+        #testing seeded data, assuming no more data added between the given dates
+        user_id = 1
+        date_start = "2025/02/01"
+        date_end = "2025/02/14"
+        result = select_activity_data(user_id, date_start, date_end)
+        assert len(result) == 2
+        assert result[0]["id"] == 9
+        assert result[0]['distance_km'] == 5.59
+        assert result[0]['moving_time'] == '00:38:57'
+        assert result[0]['date'] == '2025/02/10'
